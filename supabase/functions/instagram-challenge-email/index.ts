172ee221922e95b5c8954d1660153d14777f13c8
Buck Chancey <blake@roland.id.au>
2025-10-08 08:24:22 +1000

    Fix cors​Headers typo and update migration naming

--- supabase/functions/instagram-challenge-email/index.ts
+++ supabase/functions/instagram-challenge-email/index.ts
@@ -8,7 +8,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import { Logger } from '../_shared/logger.ts'
 import { DiscordLogger } from '../_shared/discord-logger.ts'
 
-const cors​Headers = {
+const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
